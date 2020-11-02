2eca3dbaca9522d420602bc8de6e54938154c699
Jerome Würf <scharom@pm.me>
2020-11-02 23:02:28 +0100

    implemented count_live_cells

--- game_of_life.r
+++ game_of_life.r
@@ -6,10 +6,28 @@ create_world <- function(length, width, initial_living_cell_ratio) {
     
     world = matrix(0L, nrow = length, ncol = width)
     world = apply(world, c(1,2), function(x) sample(c(0,1),1,TRUE, c(1-initial_living_cell_ratio, initial_living_cell_ratio)))
-    print(sum(world))
+    print(world)
+    world
 }
 
-do.call(create_world,options)
+count_living_cells <- function(world, x, y, neighborhood_size) {
+    print(neighborhood_size)
+    x_vals = (x-neighborhood_size):(x+neighborhood_size)
+    y_vals = (y-neighborhood_size):(y+neighborhood_size)
+    print(x_vals)
+    sum = 0
+    for(x_hat in x_vals) {
+        for(y_hat in y_vals) {
+            if (x_hat == x & y_hat == y) {
+                next
+            }
+            sum = sum + world[x_hat,y_hat]
+        }   
+    }
+    sum
+}
+
+print(count_living_cells(do.call(create_world,options), 6,6, 1))
 
 tick <- function(world) {
     iteration = 0
