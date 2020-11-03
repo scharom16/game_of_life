49992f8450a883d9e987b30e6002ac333987f237
Jerome Würf <scharom@pm.me>
2020-11-03 14:18:33 +0100

    fixed indexing of count_living_cells

--- game_of_life.r
+++ game_of_life.r
@@ -3,20 +3,29 @@ options = as.double(unlist(options))
 options = as.list(options)
 
 create_world <- function(length, width, initial_living_cell_ratio) {
-    
     world = matrix(0, nrow = length, ncol = width)
     world = apply(world, c(1,2), function(x) sample(c(0,1),1,TRUE, c(1-initial_living_cell_ratio, initial_living_cell_ratio)))
-    print(world)
     world
 }
 
+calc_index <-function(base, value,  neighborhood_size=0, index_shift=1) {
+    (((base + value - neighborhood_size - index_shift) %% base) + index_shift)
+}
+
+create_lower_bounds <- function(x, y, nrows, ncols, neighborhood_size=1, index_shift=1) {
+    c(calc_index(nrows,x,1), calc_index(ncols,y,1))
+}
+
 count_living_cells <- function(world, x, y, neighborhood_size) {
-    print(neighborhood_size)
-    x_vals = (x-neighborhood_size):(x+neighborhood_size)
-    y_vals = (y-neighborhood_size):(y+neighborhood_size)
+    ncols = ncol(world)
+    nrows = nrow(world)
+    lower_bounds = create_lower_bounds(x, y, nrows, ncols)
     sum = 0
-    for(x_hat in x_vals) {
-        for(y_hat in y_vals) {
+    interval_lenght = neighborhood_size * 2
+    for(x_hat in lower_bounds[1]:(lower_bounds[1]+interval_lenght)) {
+        x_hat = calc_index(nrows, x_hat)
+        for(y_hat in lower_bounds[2]:(lower_bounds[2]+interval_lenght)) {
+            y_hat = calc_index(ncols, y_hat)
             if (x_hat == x & y_hat == y) {
                 next
             }
@@ -26,24 +35,23 @@ count_living_cells <- function(world, x, y, neighborhood_size) {
     sum
 }
 
-print(count_living_cells(do.call(create_world,options), 6,6, 1))
-
-tick <- function(world) {
-    alive = world.sum()
-    new_world = Matrix(0,nrow(world),ncol(world))
+tick <- function(world, neighborhood_size) {
+    new_world = matrix(0,nrow(world),ncol(world))
     for (x in 1:nrow(world)) {
         for (y in 1:ncol(world)) {
-            neighbor_count = count_living_cells(world, x, y, 1)
+            neighbor_count = count_living_cells(world, x, y, neighborhood_size)
             # is cell alive?
-            if(world[x,y] == 1){
+            alive = world[x,y] == 1
+            # print(sprintf('alive: %s', alive))
+            if (alive) {
                 # is neigbourhood over or under populated
-                if (neighbor_count < 2 | 3 < neighbor_count) {
+                if ((neighbor_count < 2) || (3 < neighbor_count)) {
                     new_world[x,y] = 0
                 } else {
-                    new_worldp[x,y] = 1
+                    new_world[x,y] = 1
                 }
-                # cell is dead and is reproduction possible
-            } else if (world[x,y] == 0 & neighbor_count == 3){
+            # cell is dead and is reproduction possible
+            } else if (! alive && (neighbor_count == 3)) {
                 new_world[x,y] = 1
             }
         }
