7dc9cffdc6d32704a91e2ba4ffbba2722599b311
Jerome Würf <scharom@pm.me>
2020-11-22 20:55:09 +0100

    Gui Prototype finished

--- server.R
+++ server.R
@@ -1,28 +1,136 @@
 library(png)
+library(plyr)
+library(doMC)
+
+registerDoMC(8)
+
+state = NULL
+
 create_world <- function(height, width, living_cell_ratio) {
     world = matrix(0, nrow = height, ncol = width)
-    world = apply(world, c(1,2), function(x) sample(c(0,1),1,TRUE, c(living_cell_ratio, 1-living_cell_ratio)))
-    world
+    apply(world, c(1,2), function(x) sample(c(0,1),1,TRUE, c(1-living_cell_ratio, living_cell_ratio)))
 }
 
-function(input, output, session) {
-    output$gameOfLifeState <- renderImage({
-        height = input$height
-        width  = input$width
-        uiwidth  <- session$clientData$output_gameOfLifeState_width
-        pixelratio <- session$clientData$pixelratio
-        livingCellRatio = input$livingCellRatio
-        outfile = tempfile(fileext = ".png")
-        pic = create_world(height, height, livingCellRatio)
+calc_index <-function(base, value,  neighborhood_size=0, index_shift=1) {
+    (((base + value - neighborhood_size - index_shift) %% base) + index_shift)
+}
 
-        png(outfile, width=uiwidth, height=uiwidth)
-        image(pic, col=gray.colors(2), xaxt='n', yaxt='n')
-        dev.off()
-        # writePNG(pic, target = outfile, dpi=100000000000)
-        list(src = outfile,
+create_lower_bounds <- function(x, y, nrows, ncols, neighborhood_size=1, index_shift=1) {
+    c(calc_index(nrows,x,1), calc_index(ncols,y,1))
+}
+
+count_living_cells <- function(world, x, y, neighborhood_size) {
+    ncols = ncol(world)
+    nrows = nrow(world)
+    lower_bounds = create_lower_bounds(x, y, nrows, ncols)
+    sum = 0
+    interval_lenght = neighborhood_size * 2
+    for(x_hat in lower_bounds[1]:(lower_bounds[1]+interval_lenght)) {
+        x_hat = calc_index(nrows, x_hat)
+        for(y_hat in lower_bounds[2]:(lower_bounds[2]+interval_lenght)) {
+            y_hat = calc_index(ncols, y_hat)
+            if (x_hat == x & y_hat == y) {
+                next
+            }
+            sum = sum + world[x_hat,y_hat]
+        }   
+    }
+    sum
+}
+
+getValue <- function(world, x, y, neighborhoodSize, birth, survival) {
+        value = 0
+        neighbor_count = count_living_cells(world, x, y, neighborhoodSize)
+        # is cell alive?
+        alive = world[x,y] == 1
+        # print(sprintf('alive: %s', alive))
+        if (alive) {
+            # is neigbourhood over or under populated
+            if ((neighbor_count < survival) || (birth < neighbor_count)) {
+                value = 0
+            } else {
+                value = 1
+            }
+        # cell is dead and is reproduction possible
+        } else if (!alive && (neighbor_count == birth)) {
+            value = 1
+        }
+        value
+}
+
+tick <- function(world, neighborhoodSize, birth, survival) {
+    newWorld = laply(1:nrow(world), function(i) {
+        t <- laply(1:ncol(world), function(j) {
+            getValue(world, i, j, neighborhoodSize, birth, survival)
+        })
+        matrix(c(rep(0,nrow(world)-length(t)), t))
+    }, .parallel = TRUE)
+    newWorld
+}
+
+init_game_of_life <- function(input) {
+    list(iter=0, world=create_world(input$matrixSize, input$matrixSize, input$livingCellRatio))
+}
+
+generate_out_file <- function(session) {
+    uiwidth  <- session$clientData$output_gameOfLifeState_width
+    pixelratio <- session$clientData$pixelratio
+    outfile = tempfile(fileext = ".png")
+    png(outfile, width=uiwidth, height=uiwidth)
+    if(!is.null(state)) {
+        title = sprintf('Iteration: %s, Alive: %s', state$iter, sum(state$world))
+        image(state$world, col=gray.colors(2, rev = TRUE), xaxt='n', yaxt='n', main=title)
+    } else {
+        plot.new()
+        text(.5,.9, '@jwuerf Fun FUn FUN @jwuerf', cex=2, col=rgb(.2,.2,.2,.7))
+    }
+    dev.off()
+    outfile
+}
+
+
+
+function(input, output, session) {
+    rv <- reactiveValues(is_running = FALSE, restart=FALSE, state=NULL)
+
+    observeEvent(input$startStop, {
+        rv$is_running = !rv$is_running
+        if(rv$is_running) {
+            updateActionButton(session, 'startStop', label = "Stop", icon('stop'))
+        } else {
+            updateActionButton(session, 'startStop', label = "Start", icon('play'))
+        }
+    })
+
+    observeEvent(input$restart, {
+        rv$restart = TRUE
+        if(rv$is_running) {
+            rv$is_running = !rv$is_running
+            updateActionButton(session, 'startStop', label = "Start", icon('play'))
+        }
+    })
+
+
+
+    output$gameOfLifeState <- renderImage({
+
+        maxIter=input$maxIteration
+        speed=input$speed
+        birth=input$birth
+        survival=input$survival
+        neighborhoodSize=input$neighborhoodSize
+        if(is.null(state) || rv$restart) {
+            state <<- init_game_of_life(input)
+            rv$restart = FALSE
+        }
+        if(rv$is_running && !is.null(state) && (state$iter < maxIter)) {
+            state <<- list(iter=state$iter+1, world=tick(state$world, neighborhoodSize, birth, survival))
+            invalidateLater(speed, session)
+        }
+        list(src = generate_out_file(session), 
          contentType = "image/png",
-         width = uiwidth,
-         height = uiwidth,
+         width = session$clientData$output_gameOfLifeState_width,
+         height = session$clientData$output_gameOfLifeState_width,
          alt = "Game of life current state")
     }, deleteFile=TRUE)
 }
--- ui.R
+++ ui.R
@@ -1,14 +1,26 @@
 fluidPage(
+    tags$head(tags$script(src = "message-handler.js")),
     HTML("<link rel=\"icon\" type=\"image/gif\" href=\"glider.gif\" />"),
     title = 'Game of life',
-    titlePanel('Game of life'),
+    titlePanel(h1('Game of life',align = 'center')),
     sidebarLayout(
         position = "right",
         sidebarPanel(
-            textInput("text", h4("Rulestring(B/S):"), value = '3/2'),
-            numericInput("width", h4("Width:"),value = 100),
-            numericInput("height", h4("Height:"), value = 100),
-            sliderInput("livingCellRatio", h4('Initial (living) cell ratio:'), min = 0, max=1, value=0.4)
+            sliderInput("neighborhoodSize", h4("Neigbourhood:"), min = 1, max=4,value = 1),
+            helpText('Defines the size of neigbourhood around a cell.'),
+            sliderInput("birth", h4("Birth:"), min = 2, max=7,value = 3),
+            helpText('Amount of neigbours needed to create a new cell.'),
+            sliderInput("survival", h4("Survival:"), min = 1, max=5,value = 2),
+            helpText('Amount of neigbours needed so that existing cell survives.'),
+            sliderInput("matrixSize", h4("Size:"),min = 10, max=200,value = 100),
+            helpText('Size of the game board.'),
+            sliderInput("livingCellRatio", h4('Starting ratio:'), min = 0.1, max=0.9, value=0.4),
+            helpText('Proportion of cells that are alive relative to the size of the board.'),
+            sliderInput("maxIteration", h4('Max Iteration:'), min = 50, max=200, value=50),
+            sliderInput('speed', h4('Speed:'), min =100, max=500, value=250),
+            helpText('Time in miliseconds to show next state.'),
+            actionButton("startStop" ,"Start", icon("play")),
+            actionButton("restart" ,"Restart", icon("redo"))
         ),
         mainPanel(
             imageOutput('gameOfLifeState')
